a104f2344b4ffe2c9b62fb6a5c161a05bc83fb1a
Tamara Gorobets <osttam89@gmail.com>
2013-01-14 10:12:10 +0400

    Created layout and design for two pages.

---
+++ MyBlog/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Post/
+
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MyBlog/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            return View();
+        }
+    }
+}
